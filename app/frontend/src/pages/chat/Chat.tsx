a699fc179be237178202fd78fefbe546e724944e
Sugam Manocha <sugam.manocha@gfm247.com>
2023-09-23 02:02:18 -0400

    update chat.tx

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -181,7 +181,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g. Did Mohammad visit Mecca on May 12th, 613 A.D. ?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
